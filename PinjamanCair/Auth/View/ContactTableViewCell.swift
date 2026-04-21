58cd0b3ae899404064bb8047553081182f15b6be
hekang <hekang@hekangs-MacBook-Air.local>
2026-04-21 15:42:53 +0800

    ccc-ui

---
+++ PinjamanCair/Auth/View/ContactTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContactTableViewCell.swift
+//  PinjamanCair
+//
+//  Created by hekang on 2026/4/21.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
